4046cbcd47ebb02c4f0c642341b7e1cd5bb6058e
PhreeXhow <51435699+PhreeXhow@users.noreply.github.com>
2021-01-11 00:41:08 -0800

    Update main.c
    Changed SIFFS to LittleFS

--- src/main.c
+++ src/main.c
@@ -100,7 +100,7 @@ void setup() {
   // if it does not connect it starts an access point with the specified name
   // here  "AutoConnectAP"
   // and goes into a blocking loop awaiting configuration
-  wifiManager.autoConnect("Con");
+  wifiManager.autoConnect("Connect MCU to Wifi");
   // or use this for auto generated name ESP + ChipID
   //wifiManager.autoConnect();
   
